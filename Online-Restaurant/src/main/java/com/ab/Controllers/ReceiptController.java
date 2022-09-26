6c60a46fe4a2f5aa41474c09b98881214e2cd5ce
Kumori7 <73298685+Kumori7@users.noreply.github.com>
2022-09-26 12:01:21 +0100

    Add files via upload

--- Online-Restaurant/src/main/java/com/ab/Controllers/ReceiptController.java
+++ Online-Restaurant/src/main/java/com/ab/Controllers/ReceiptController.java
@@ -1,10 +1,13 @@
 package com.ab.Controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ab.Entities.Receipt;
 import com.ab.Services.BasketServiceImpl;
 import com.ab.Services.CheckOutServiceImpl;
 import com.ab.Services.ReceiptServiceImpl;
@@ -33,12 +36,23 @@ public class ReceiptController {
 		if (totalCost <= 0 && orderQuantity <= 0) {
 			// if total cost is zero need to
 			// stop it saving to db or delete it
+			// so a receipt with zero items is not created
 			return "empty_basket_failed_order";
 		} else {
-			return "test";
+			return "Order_success";
 
 		}
 
+	}
+
+	@RequestMapping("/view-order-history")
+	public String viewOrderHistory(Model model) {
+
+		List<Receipt> receipts = receiptServiceImpl.displayReceipts();
+
+		model.addAttribute("receiptsList", receipts);
+
+		return "view_order_history";
 	}
 
 } // end of class
